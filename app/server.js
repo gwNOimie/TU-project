82c005255e10e23c418e86f03ea60cc3f19bf362
gaetank <gaetan.kemayou@hotmail.fr>
2018-01-17 14:31:34 +0100

    ajout de la page d'inscription

--- app/server.js
+++ app/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.set('views', `${__dirname}/views`);
+app.set('views', `views`);
 app.set('view engine', 'twig');
 
 // This section is optional and can be used to configure twig.
 app.set('twig options', {
   strict_variables: false
 });
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/register', function(req, res){
+    res.render('register')
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'))

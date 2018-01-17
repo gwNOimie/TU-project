ffb1af6c0c0fcd0e2e3238ee00506b4a73ec221a
gwNO <gwenael.henot@gmail.com>
2018-01-17 11:24:47 +0100

    init login route/ctrl

---
+++ app/controllers/indexController.js
@@ -0,0 +1,15 @@
+class IndexController {
+  index(req, res) {
+    return null;
+  }
+
+  login(req, res) {
+    return null;
+  }
+
+  register(req, res) {
+    return null;
+  }
+}
+
+module.exports = IndexController;
---
+++ test/controllers/indexControllerSpec.js
@@ -0,0 +1,8 @@
+const expect = require('expect');
+const IndexCtrl = require('../../app/controllers/indexController');
+
+describe("indexController", () => {
+  describe("#index", () => { });
+  describe("#login", () => { });
+  describe("#register", () => { });
+});

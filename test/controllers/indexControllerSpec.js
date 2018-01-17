0fa325527b5a56a75cd4b15aa22862eba8a4e237
gwNO <gwenael.henot@gmail.com>
2018-01-17 11:33:06 +0100

    WIP: test structure

--- test/controllers/indexControllerSpec.js
+++ test/controllers/indexControllerSpec.js
@@ -3,6 +3,14 @@ const IndexCtrl = require('../../app/controllers/indexController');
 
 describe("indexController", () => {
   describe("#index", () => { });
-  describe("#login", () => { });
+  describe("#login", () => {
+    it("Vérification du paramètre", () => { });
+    it("", () => {
+      // arrange
+      // act
+      // assert
+    });
+    it("", () => { });
+  });
   describe("#register", () => { });
 });
